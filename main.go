770fad2ec2c242ddbe45fcf6af11ba4c7ac7dabf
Rick Nunes <sanunes.ricardo@gmail.com>
2019-03-04 20:30:18 -0300

    All files

---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"path/filepath"
+	"sync"
+	"text/template"
+)
+
+func main() {
+	http.Handle("/", &templateHandler{filename: "chat.html"})
+
+	// start the web server
+	if err := http.ListenAndServe(":8081", nil); err != nil {
+		log.Fatal("ListenAndServe:", err)
+	}
+}
+
+// temp1 to represent a single template
+type templateHandler struct {
+	once     sync.Once
+	filename string
+	temp1    *template.Template
+}
+
+// ServeHTTP method handles the HTTP request .
+func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	t.once.Do(func() {
+		t.temp1 = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
+	})
+	t.temp1.Execute(w, nil)
+}
