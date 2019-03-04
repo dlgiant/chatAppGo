db68fced83ed5d3af4dbbbbe4d1d9b4ebad8bc5c
Rick Nunes <sanunes.ricardo@gmail.com>
2019-03-04 20:59:19 -0300

    Debuged room

--- room.go
+++ room.go
@@ -1,5 +1,12 @@
 package main
 
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/websocket"
+)
+
 type room struct {
 	forward chan []byte
 	join    chan *client
@@ -23,14 +30,14 @@ func (r *room) run() {
 	}
 }
 
-const {
-	socketBufferSize = 1024
+const (
+	socketBufferSize  = 1024
 	messageBufferSize = 256
-}
+)
 
-var upgrader = &websocket.Upgrader(ReadBufferSize: socketBufferSize, WriteBufferSize: socketBufferSize)
+var upgrader = &websocket.Upgrader{ReadBufferSize: socketBufferSize, WriteBufferSize: socketBufferSize}
 
-func (r *room) ServeHTTP(w httpResponseWriter, req *http.Request) {
+func (r *room) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 	socket, err := upgrader.Upgrade(w, req, nil)
 	if err != nil {
 		log.Fatal("ServeHTTP:", err)
@@ -38,8 +45,8 @@ func (r *room) ServeHTTP(w httpResponseWriter, req *http.Request) {
 	}
 	client := &client{
 		socket: socket,
-		send: make(chan []byte, messageBufferSize),
-		room: r,
+		send:   make(chan []byte, messageBufferSize),
+		room:   r,
 	}
 	r.join <- client
 	defer func() { r.leave <- client }()
@@ -50,8 +57,8 @@ func (r *room) ServeHTTP(w httpResponseWriter, req *http.Request) {
 func newRoom() *room {
 	return &room{
 		forward: make(chan []byte),
-		join: make(chan *client),
-		leave: make(chan *client),
+		join:    make(chan *client),
+		leave:   make(chan *client),
 		clients: make(map[*client]bool),
 	}
 }
