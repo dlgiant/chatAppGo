d49264335e8f05a0a2e29fb1eaf4af9968159364
Ricardo Nunes <sanunes.ricardo@gmail.com>
2019-03-05 16:28:53 -0500

    Added Authentication

---
+++ auth.go
@@ -0,0 +1,29 @@
+package main
+import (
+	"net/http"
+)
+
+type authHandler struct {
+	next http.Handler
+}
+
+func (h *authHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	_, err := r.Cookie("auth")
+	if err == http.ErrNoCookie {
+		// User was not authenticated
+		w.Header().Set("Location", "/login")
+		w.WriteHeader(http.StatusTemporaryRedirect)
+		return
+	}
+	if err != nil {
+		// Handle any other error
+		http.Error(w, err.Error(), http.StatusInternalServiceError)
+		return
+	}
+	// In case of success, call the next handler
+	h.next.ServeHTTP(w, r)
+}
+
+func MustAuth(hd http.Handler) http.Handler {
+	return &authHandler{next: hd}
+}
