c0eaf23fe3e7ee0a726a89cf9f9a4a0f4710cfa9
Ricardo Nunes <sanunes.ricardo@gmail.com>
2019-03-06 21:49:58 -0500

    Added loginHandler to deal with third party login

--- auth.go
+++ auth.go
@@ -1,6 +1,10 @@
 package main
+
 import (
+	"fmt"
+	"log"
 	"net/http"
+	"strings"
 )
 
 type authHandler struct {
@@ -26,4 +30,19 @@ func (h *authHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
 func MustAuth(hd http.Handler) http.Handler {
 	return &authHandler{next: hd}
+}
+
+// loginHandler deals with third-party login
+// format: /auth/{action}/{provider}
+func loginHandler(w http.ResponseWriter, r *http.Request) {
+	segs := strings.Split(r.URL.Path, "/")
+	action := segs[2]
+	provider := segs[3]
+	switch action {
+	case "login":
+		log.Println("TODO handle login for", provider)
+	default:
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprintf(w, "Auth action %s not supported", action)
+	}
 }
